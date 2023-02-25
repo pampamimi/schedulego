a7b227af93188b1aea8b94e1e97382e65f93712c
Varnilla <reidev241@gmail.com>
2023-02-25 19:05:11 +0700

    🔨Small fixes

--- index.js
+++ index.js
@@ -1,27 +1,21 @@
 const qrcode = require('qrcode-terminal');
 
-const { Client } = require('whatsapp-web.js');
-const client = new Client();
+const { Client, LocalAuth } = require('whatsapp-web.js');
 
-// const client = new Client({
-//      authStrategy: new LocalAuth({
-//           clientId: "client-one"
-//      })
-// })
+const client = new Client({
+     authStrategy: new LocalAuth()
+})
 
-// to commit qr code when not logged in
 client.on("qr", qr => {
     qrcode.generate(qr, {small: true} );
 })
 
-// ready event
 client.on('ready', () => {
     console.log("ScheduLEGO has logged in!")
 });
 
 client.initialize();
-
-// message event
+ 
 client.on('message', message => {
 	if(message.body === '!ping') {
 		message.reply('pong');
