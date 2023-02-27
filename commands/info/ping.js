c70dda340e7849bcd9a12ce21aefaca73a01c544
Varnilla <reidev241@gmail.com>
2023-02-26 18:10:15 -0800

    ✨ Command Handler

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -1,9 +1,6 @@
 module.exports = {
     name: "ping",
     run: async (client, message, args) => {
-        console.log(client)
-        console.log(args)
-        console.log(client.ws)
         client.sendMessage(message.from, 'pong');
     },
 }
