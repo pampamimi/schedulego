8decb2c580947ad5fad3c265856ef75a62b7b208
Varnilla <reidev241@gmail.com>
2023-03-01 20:53:32 +0700

    🔨 Removed alias (temporarily?) due to error

--- events/message.js
+++ events/message.js
@@ -6,10 +6,23 @@ client.on("message", async (message) => {
 
     const [cmd, ...args] = message.body.slice(client.config.prefix.length).trim().split(/ +/g)
 
-    const command = client.commands.get(cmd.toLowerCase()) || client.commands.find(c => c.aliases?.includes(cmd.toLowerCase()))
+    const command = client.commands.get(cmd.toLowerCase())
 
     if (!command) return
 
     await command.run(client, message, args)
     
 })
+
+
+// client.commands.find(c => c.aliases?.includes(cmd.toLowerCase()))
+
+/* Example of "find" methods, scrapped from Discord.Collection (discord.js)
+  find(fn, thisArg) {
+    if (typeof thisArg !== "undefined")
+      fn = fn.bind(thisArg);
+    for (const [key, val] of this) {
+      if (fn(val, key, this))
+        return val;
+    }
+*/
