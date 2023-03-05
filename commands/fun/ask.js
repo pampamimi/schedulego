5141dcb848ba6e4ba1ebee829ea27be00c24a781
Varnilla <reidev241@gmail.com>
2023-03-05 19:07:11 +0700

    🧨 demoknighttf2

--- commands/fun/ask.js
+++ commands/fun/ask.js
@@ -1,12 +1,12 @@
 const response =
 ["Mimo setuju!", "Kayaknya iya?", "Iyaa", "Mungkin?", "Yapp",
-"Coba tanya Mimo lagi next time", "Mimo gak bisa jawab buat sekarang...", "Menurut Mimo... Gatau deh.", "Mimo gak mau jawab", "Terserah kamu ajaa, Mimo bingung",
+"Coba tanya Mimo lagi next time", "Mimo gak bisa jawab buat sekarang...", "Menurut Mimo... Gatau deh.", "Mimo gak mau jawab 🐒", "Terserah kamu ajaa, Mimo bingung",
 "NGGAK!", "Mimo gak setuju", "Mimo gak tauu :(", "Tentunya enggak", "Mimo kurang setuju..."]
 
 module.exports = {
     name: "ask",
     run: async (client, message, args) => {
-        if (!args[0]) return message.reply("Tanya Mimo pertanyaan yang jawabannya bisa positif atau negatif!")
+        if (!args[0]) return message.reply("Kamu mau nanya mimo apaa? 🤔")
         message.reply(response[Math.floor(Math.random() * response.length)])
     },
 }
